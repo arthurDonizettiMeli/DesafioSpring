bc5be80a3bf3bc402c39632727b17a755a17b7ad
YanBroetto <yan.broetto@mercadolivre.com>
2021-07-06 17:14:05 -0300

    Criado model de produto

--- src/main/java/br/com/meli/desafiospring/models/Product.java
+++ src/main/java/br/com/meli/desafiospring/models/Product.java
@@ -1,4 +1,87 @@
 package br.com.meli.desafiospring.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 5, max = 40)
+    @NotNull
+    private String name;
+
+    @Size(max = 255)
+    @NotNull
+    private String type;
+
+    @Size(max = 255)
+    @NotNull
+    private String brand;
+
+    @Size(max = 255)
+    @NotNull
+    private String color;
+
+    @Size(max = 255)
+    @NotNull
+    private String notes;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
--- src/main/java/br/com/meli/desafiospring/models/User.java
+++ src/main/java/br/com/meli/desafiospring/models/User.java
@@ -13,7 +13,6 @@ public class User {
 
     private static final long serialVersionUID = 1L;
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
