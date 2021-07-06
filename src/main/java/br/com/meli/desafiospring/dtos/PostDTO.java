8010446c92a2a9bee847da3c0965ce410a8c8ccd
MichelAndrew <michel.dasilva@mercadolivre.com>
2021-07-06 19:03:18 -0300

    Criacao das DTOs, Post service e Product controller

---
+++ src/main/java/br/com/meli/desafiospring/dtos/PostDTO.java
@@ -0,0 +1,82 @@
+package br.com.meli.desafiospring.dtos;
+
+import java.util.Date;
+
+public class PostDTO {
+
+    private Integer userID;
+    private Integer id_post;
+    private Date date;
+    private ProductDTO detail;
+    private Integer category;
+    private Double price;
+    private Boolean hasPromo;
+    private Double discount;
+
+    public PostDTO() {
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getId_post() {
+        return id_post;
+    }
+
+    public void setId_post(int id_post) {
+        this.id_post = id_post;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public ProductDTO getDetail() {
+        return detail;
+    }
+
+    public void setDetail(ProductDTO detail) {
+        this.detail = detail;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Boolean getHasPromo() {
+        return hasPromo;
+    }
+
+    public void setHasPromo(Boolean hasPromo) {
+        this.hasPromo = hasPromo;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+}
