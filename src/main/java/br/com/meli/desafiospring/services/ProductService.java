55538b5cd137d4bee97793956f75e57bc4da7298
YanBroetto <yan.broetto@mercadolivre.com>
2021-07-06 17:16:21 -0300

    Criado service de produto

--- src/main/java/br/com/meli/desafiospring/services/ProductService.java
+++ src/main/java/br/com/meli/desafiospring/services/ProductService.java
@@ -1,4 +1,13 @@
 package br.com.meli.desafiospring.services;
 
+import br.com.meli.desafiospring.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
 }
