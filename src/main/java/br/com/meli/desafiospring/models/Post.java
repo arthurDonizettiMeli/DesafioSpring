d895d02d54ac4752271296159025916d26477c5d
arthurDonizettiMeli <arthur.donizetti@mercadolivre.com>
2021-07-06 17:57:16 -0300

    Implementação do post model

--- src/main/java/br/com/meli/desafiospring/models/Post.java
+++ src/main/java/br/com/meli/desafiospring/models/Post.java
@@ -1,4 +1,58 @@
 package br.com.meli.desafiospring.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NotNull
+  private Integer userId;
+
+  @NotNull
+  private LocalDate date;
+
+  private List<Product> detail;
+
+  @OneToMany(targetEntity=Product.class, mappedBy="id",cascade=CascadeType.ALL, fetch = FetchType.LAZY)
+  public List<Product> getDetail() {
+    return detail;
+  }
+
+  public void setDetail(List<Product> detail) {
+    this.detail = detail;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Integer getUserId() {
+    return userId;
+  }
+
+  public void setUserId(Integer userId) {
+    this.userId = userId;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
 }
--- src/main/java/br/com/meli/desafiospring/repositories/PostRepository.java
+++ src/main/java/br/com/meli/desafiospring/repositories/PostRepository.java
@@ -1,4 +1,9 @@
 package br.com.meli.desafiospring.repositories;
 
-public class PostRepository {
+import br.com.meli.desafiospring.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
 }
