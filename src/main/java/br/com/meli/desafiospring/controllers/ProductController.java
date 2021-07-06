7a4c3fb9f35914812d64d630cd916380ae278a36
YanBroetto <yan.broetto@mercadolivre.com>
2021-07-06 17:02:26 -0300

    Criado controller de produto

---
+++ src/main/java/br/com/meli/desafiospring/controllers/ProductController.java
@@ -0,0 +1,9 @@
+package br.com.meli.desafiospring.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+}
