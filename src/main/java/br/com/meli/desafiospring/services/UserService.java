ebacc3610a358be5d28c2756a50b5719f8426519
arthurDonizettiMeli <arthur.donizetti@mercadolivre.com>
2021-07-07 10:49:56 -0300

    Implementado método de unfollow

--- src/main/java/br/com/meli/desafiospring/services/UserService.java
+++ src/main/java/br/com/meli/desafiospring/services/UserService.java
@@ -7,7 +7,6 @@ import br.com.meli.desafiospring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,5 +34,24 @@ public class UserService {
 
     public UserFollowers follow(UserFollowers userFollowers) {
         return userFollowersRepository.save(userFollowers);
+    }
+
+    public boolean unfollow(Integer followerId, Integer followedId) {
+        Optional<User> optionalFollower = userRepository.findById(followerId);
+        if (optionalFollower.isPresent() == false)
+            return false;
+
+        Optional<User> optionalFollowed = userRepository.findById(followedId);
+        if (optionalFollowed.isPresent() == false)
+            return false;
+
+        Optional<UserFollowers> optionalFollow = userFollowersRepository.getUserFollowersByFollowerIdAndFollowedId(followerId, followedId);
+        if (optionalFollow.isPresent() == false)
+            return false;
+
+        UserFollowers follow = optionalFollow.get();
+
+        userFollowersRepository.delete(follow);
+        return true;
     }
 }
