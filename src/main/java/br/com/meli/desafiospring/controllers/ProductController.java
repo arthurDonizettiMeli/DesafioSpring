ad36313be30b2b92ed34a6ca86ba7b16982197e9
YanBroetto <yan.broetto@mercadolivre.com>
2021-07-07 09:27:44 -0300

    Iniciado os metodos da classe PostService

--- src/main/java/br/com/meli/desafiospring/controllers/ProductController.java
+++ src/main/java/br/com/meli/desafiospring/controllers/ProductController.java
@@ -2,7 +2,7 @@ package br.com.meli.desafiospring.controllers;
 
 import br.com.meli.desafiospring.dtos.PostDTO;
 import br.com.meli.desafiospring.dtos.ProductDTO;
-import br.com.meli.desafiospring.services.ProductService;
+import br.com.meli.desafiospring.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +13,14 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
-//    @Autowired
-//    ProductService productService;
+    @Autowired
+    PostService postService;
 
     @PostMapping(value = "/newpost")
     public ResponseEntity<ProductDTO> CreatePost(@RequestBody PostDTO postDTO) {
         try {
 
-            PostDTO post = postService.createPost;
+            PostDTO post = postService.createPost();
 
             return ResponseEntity.ok(post.getDetail());
 
@@ -28,22 +28,6 @@ public class ProductController {
             return ResponseEntity.status(400).build();
         }
 
-    }
-
-    @GetMapping(value = "/followed/{userId}/list")
-    public ResponseEntity<List<PostDTO>> GetPostsByUser(@PathVariable(value = "userId") int userId) {
-
-        List<PostDTO> postList = postService.getById(userId);
-
-        return ResponseEntity.ok(postList);
-    }
-
-    @GetMapping(value = "/followed/{userId}/list")
-    public ResponseEntity<List<PostDTO>> GetPostsByUser(@PathVariable(value = "userId") int userId) {
-
-        List<PostDTO> postList = postService.getById(userId);
-
-        return ResponseEntity.ok(postList);
     }
 
     @GetMapping(value = "/followed/{userId}/list")
