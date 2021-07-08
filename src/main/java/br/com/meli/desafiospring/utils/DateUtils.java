d56ad1bcf2cad7e7371b305d7d5ca08c5a8f9231
YanBroetto <yan.broetto@mercadolivre.com>
2021-07-08 15:44:14 -0300

    Adicionado comentarios nos metodos do pacote utils

--- src/main/java/br/com/meli/desafiospring/utils/DateUtils.java
+++ src/main/java/br/com/meli/desafiospring/utils/DateUtils.java
@@ -4,6 +4,12 @@ import java.util.concurrent.TimeUnit;
 
 public class DateUtils {
 
+    /**
+     * Obtem a diferença entre duas êpocas em dias
+     * @param startTime Êpoca inicial
+     * @param endTime Êpoca final
+     * @return diferença entre as duas épocas em dias
+     */
     public static long getDifferenceBetweenEpochsInDays(long startTime, long endTime) {
         long difference = endTime - startTime;
         return TimeUnit.MILLISECONDS.toDays(difference);
--- src/main/java/br/com/meli/desafiospring/utils/SortUtils.java
+++ src/main/java/br/com/meli/desafiospring/utils/SortUtils.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class SortUtils {
 
+    /**
+     * Ordena uma lista de acordo com o parametro order
+     * @param list Lista a ser ordenada
+     * @param order Descrição da ordenação
+     * @param <T> Generics do tipo da lista
+     */
     public static <T> void sort(List<T> list, String order) {
         if (order == null || order.contains("asc")) {
             list.sort(null);
