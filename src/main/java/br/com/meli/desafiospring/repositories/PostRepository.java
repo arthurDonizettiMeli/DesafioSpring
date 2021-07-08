555b7bbb101d85b74f3b1acbd00d5a829b040484
YanBroetto <yan.broetto@mercadolivre.com>
2021-07-08 15:41:03 -0300

    Refatorando: PostRepository

--- src/main/java/br/com/meli/desafiospring/repositories/PostRepository.java
+++ src/main/java/br/com/meli/desafiospring/repositories/PostRepository.java
@@ -4,11 +4,6 @@ import br.com.meli.desafiospring.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
-  //Optional<List<Post>> getPostByUserIdAAndHasPromoIsTrue(Integer userId);
-  Optional<List<Post>> getAllByUserIdAndHasPromo(Integer userId, boolean hasPromo);
 }
